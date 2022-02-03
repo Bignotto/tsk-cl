97d6270d4392dbdfc18ec75003ff4f89aab3cc3c
Thiago Bignotto <bignotto@gmail.com>
2022-02-03 20:15:49 -0300

    implementing file tasks repository save commit

--- src/repositories/FileTasksRepository.js
+++ src/repositories/FileTasksRepository.js
@@ -1,12 +1,34 @@
 import fs from "fs";
+import { CliError } from "../shared/erros/CliError";
 
 class FileTasksRepository {
   constructor() {}
+
+  async parseFile(filePath) {
+    const exist = await fs.existsSync(filePath);
+    if (!exist) {
+      await fs.writeFileSync(filePath, "0\n");
+    }
+
+    const linesArray = fs.readFileSync(filePath).toString().split("\n");
+    const taskCount = parseInt(linesArray[0]);
+    if (taskCount !== taskCount)
+      throw new CliError("something wrong", "tasks file looks wierd");
+  }
+
   async create({ description, priority }) {
-    //TODO: move this to constructor
-    const exist = await fs.existsSync(".tasks");
-    if (exist) return true;
-    return false;
+    await this.parseFile(".tasks");
+
+    // const lines = await fs.readFileSync(".tasks").toString();
+    // const task = {
+    //   id: lines.split("\n").length,
+    //   description,
+    //   created: Date.now(),
+    //   status: "pending",
+    //   priority,
+    // };
+
+    // await fs.appendFileSync(".tasks", `${JSON.stringify(task)}\n`);
   }
 }
 
