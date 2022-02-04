db7b2b321098c488e84524bbc2aa9e26a736f0c6
Thiago Bignotto <bignotto@gmail.com>
2022-02-03 21:03:38 -0300

    implementing file tasks repository save commit

--- src/repositories/FileTasksRepository.js
+++ src/repositories/FileTasksRepository.js
@@ -1,32 +1,47 @@
 import fs from "fs";
-import { CliError } from "../shared/erros/CliError";
+import { CliError } from "../shared/erros/CliError.js";
 
 class FileTasksRepository {
-  constructor() {}
+  constructor() {
+    this.tasks = [];
+  }
 
   async parseFile(filePath) {
     const exist = await fs.existsSync(filePath);
     if (!exist) {
-      await fs.writeFileSync(filePath, "0\n");
+      await fs.writeFileSync(filePath, "");
     }
 
     const linesArray = fs.readFileSync(filePath).toString().split("\n");
-    const taskCount = parseInt(linesArray[0]);
-    if (taskCount !== taskCount)
-      throw new CliError("something wrong", "tasks file looks wierd");
+    // const taskCount = parseInt(linesArray[0]);
+    // if (taskCount !== taskCount)
+    //   throw new CliError("something wrong", "tasks file looks wierd");
+    this.tasks = linesArray;
+    return linesArray;
   }
 
   async create({ description, priority }) {
-    await this.parseFile(".tasks");
+    const lines = await this.parseFile(".tasks");
 
-    // const lines = await fs.readFileSync(".tasks").toString();
-    // const task = {
-    //   id: lines.split("\n").length,
-    //   description,
-    //   created: Date.now(),
-    //   status: "pending",
-    //   priority,
-    // };
+    let nextId = 1;
+    if (lines.length > 1) nextId = lines[lines.length - 1];
+    //TODO: got to parse lines first
+
+    const task = {
+      id: taskCount + 1,
+      description,
+      created: Date.now(),
+      status: "pending",
+      priority,
+    };
+
+    this.tasks.push(task);
+
+    //TODO: task id strategy
+    // find highest id and sum 1 to make new number id
+    // save last id on file
+    // - try to save a number on the file last line
+    // - save next id on a task, so the last task will always have next id
 
     // await fs.appendFileSync(".tasks", `${JSON.stringify(task)}\n`);
   }
