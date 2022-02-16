7646f0f4a97e94c8745a2b46587df5e646dcd294
Thiago Bignotto <bignotto@gmail.com>
2022-02-15 21:33:11 -0300

    implementing next task service save commit

--- src/services/nextTask/NextTaskService.js
+++ src/services/nextTask/NextTaskService.js
@@ -5,9 +5,22 @@ class NextTaskService {
 
   async execute() {
     const tasks = await this.tasksRepository.list();
-    //TODO: implement task service
-    // find first high, first normal and first low priorities tasks
-    return tasks;
+    const pendingTasks = tasks.filter((t) => t.status === "pending");
+    console.log(pendingTasks);
+
+    const highTasks = pendingTasks.filter((t) => t.priority === "high");
+    const normalTasks = pendingTasks.filter((t) => t.priority === "normal");
+    const lowTasks = pendingTasks.filter((t) => t.priority === "low");
+
+    //TODO: fix how to return tasks
+    // should be position fixed? [0] == high, [1] == normal, [2] == low
+
+    const returnNextTasks = [];
+    if (highTasks[0]) returnNextTasks.push(highTasks[0]);
+    if (normalTasks[0]) returnNextTasks.push(normalTasks[0]);
+    if (lowTasks[0]) returnNextTasks.push(lowTasks[0]);
+
+    return returnNextTasks;
   }
 }
 
