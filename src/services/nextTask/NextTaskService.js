cba7841309f8a9d5f5c06470299b76423376469e
Thiago Bignotto <bignotto@gmail.com>
2022-02-17 21:34:23 -0300

    implemented next task command tests

--- src/services/nextTask/NextTaskService.js
+++ src/services/nextTask/NextTaskService.js
@@ -6,7 +6,6 @@ class NextTaskService {
   async execute() {
     const tasks = await this.tasksRepository.list();
     const pendingTasks = tasks.filter((t) => t.status === "pending");
-    console.log(pendingTasks);
 
     const highTasks = pendingTasks.filter((t) => t.priority === "high");
     const normalTasks = pendingTasks.filter((t) => t.priority === "normal");
--- src/services/nextTask/NextTaskService.spec.js
+++ src/services/nextTask/NextTaskService.spec.js
@@ -24,8 +24,49 @@ describe("Next Task Service", () => {
     await createTaskService.execute("this is task 1", "high");
   });
 
-  it("should list next task correctly", async () => {
+  it("should list next task correctly - all tasks", async () => {
     const nextTasks = await nextTaskService.execute();
     expect(nextTasks[0].priority).toBe("high");
+  });
+
+  it("should list next task correctly - normal task complete", async () => {
+    await completeTaskService.execute(1);
+    const nextTasks = await nextTaskService.execute();
+    expect(nextTasks.length).toBe(2);
+    expect(nextTasks[0].priority).toBe("high");
+  });
+
+  it("should list next task correctly - high task complete", async () => {
+    await completeTaskService.execute(3);
+    const nextTasks = await nextTaskService.execute();
+    expect(nextTasks.length).toBe(2);
+    expect(nextTasks[0].priority).toBe("normal");
+  });
+
+  it("should list next task correctly - more than one task on each priority", async () => {
+    await createTaskService.execute("should not be next", "normal");
+    await createTaskService.execute("should not be next", "low");
+    await createTaskService.execute("should not be next", "high");
+
+    const nextTasks = await nextTaskService.execute();
+    expect(nextTasks.length).toBe(3);
+    expect(nextTasks[0].description).toBe("this is task 1");
+  });
+
+  it("should list next task correctly - more than one task on each priority - complete high task", async () => {
+    await createTaskService.execute("should not be next", "normal");
+    await createTaskService.execute("should not be next", "low");
+    await createTaskService.execute("should be next", "high");
+
+    await completeTaskService.execute(3);
+
+    const nextTasks = await nextTaskService.execute();
+    expect(nextTasks.length).toBe(3);
+    expect(nextTasks[0].description).toBe("should be next");
+
+    await completeTaskService.execute(6);
+    const nextTasks2 = await nextTaskService.execute();
+    expect(nextTasks2.length).toBe(2);
+    expect(nextTasks2[0].priority).toBe("normal");
   });
 });
